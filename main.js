c14b9c0a925f1cf46188eae8c903e179b986aab2
Rizwan Sheikh <54903945+riz1-lv@users.noreply.github.com>
2020-08-03 19:50:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+const sweenyWords = ["SYDNEY SWEENEY", "SWEENEY", "SYDNEY",'SYD'];
 
 client.on('ready', () => {
-    console.log('Sweeny Bot is ready');
+    console.log('SweeneyBot is ready');
 });
 client.on('message', message => {
     
-    if (message.content === 'ping') {
-      
-      message.channel.send('https://tenor.com/view/sydney-sweeney-sydney-sweeney-euphoria-ice-cream-gif-14906880');
+    if (checkSweeney(message.content)) {
+        const attachment = new MessageAttachment('https://tenor.com/view/sydney-sweeney-sydney-sweeney-euphoria-ice-cream-gif-14906880');
+      message.channel.send(attachment);
     }
 });
   
 
-
+function checkSweeney(message) {
+    for (let i = 0; i < sweenyWords.length; i++){
+        if (message.toUpperCase() === sweenyWords[i]) {
+            return true;
+        }
+            
+    }
+    
+}
 
 client.login(process.env.BOT_TOKEN);
