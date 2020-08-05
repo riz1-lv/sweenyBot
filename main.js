9f6f50a7141481a6b7e93c55933bf50546a38781
Rizwan <riz.shkh1@gmail.com>
2020-08-05 15:48:16 -0400

    fixed bug caused by bugfix

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ client.on('message', message => {
     
     if (checkSweeney(message.content)) {
         
-      message.channel.send('https://rb.gy/jyn7b8');
+      message.channel.send('https://tenor.com/view/sydney-sweeney-sydney-sweeney-euphoria-ice-cream-gif-14906880');
     }
     if (message.content.includes('!jen')) {
         message.channel.send('https://i.redd.it/sywglt58ngg01.jpg');
@@ -19,11 +19,14 @@ client.on('message', message => {
   
 
 function checkSweeney(msg) {
-    for (let i = 0; i < sweenyWords.length; i++){
-        if (msg.toUpperCase().includes(sweenyWords[i])) {
-            return true;
+    if (msg === 'https://tenor.com/view/sydney-sweeney-sydney-sweeney-euphoria-ice-cream-gif-14906880')
+        return false;
+    else{
+        for (let i = 0; i < sweenyWords.length; i++) {
+            if (msg.toUpperCase().includes(sweenyWords[i])) {
+                return true;
+            }
         }
-            
     }
     return false;
 }
